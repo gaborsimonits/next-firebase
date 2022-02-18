1a9461901acf39f08cec5e34f1028711efe48556
gaborsimonits <gaborsimonits@gmail.com>
2022-02-18 12:51:41 +0100

    push navbar, usercontext, auth intro

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,19 @@
 import "../styles/globals.css";
+import Navbar from "../components/Navbar";
+import { Toaster } from "react-hot-toast";
 
 // import { GetServerSideProps } from "next/app";
 
+import { UserContext } from "../lib/context";
+
 function MyApp({ Component, pageProps }) {
-	return <Component {...pageProps} />;
+	return (
+		<UserContext.Provider value={{ user: {}, username: "jeff" }}>
+			<Navbar />
+			<Component {...pageProps} />
+			<Toaster />
+		</UserContext.Provider>
+	);
 }
 
 export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -4,10 +4,14 @@ import Link from "next/link";
 
 import Loader from "../components/Loader";
 
+import toast from "react-hot-toast";
+
 export default function Home() {
 	return (
 		<div>
 			<Loader className={styles.loader} show />
+
+			<button onClick={() => toast.success("hi toast")}>Toast Me</button>
 		</div>
 	);
 }
